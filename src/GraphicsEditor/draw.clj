26e72785262878adc645fda40314ded5a57a5ca2
Christian Blunden <christian.blunden@gmail.com>
2011-05-15 17:43:50 +0100

    refactored neighbours to use inline function

--- src/GraphicsEditor/draw.clj
+++ src/GraphicsEditor/draw.clj
@@ -2,9 +2,9 @@
 
 (def deltas [[-1 0] [1 0] [0 -1] [0 1]])
 
-(defn valid? [point]
+(defn valid? [point size]
   (every? #(<= 0 %) point))
 
-(defn neighbours [point]
-  (filter valid?
+(defn neighbours [point size]
+  (filter (fn [new-point] (valid? new-point size))
           (map #(map + point %) deltas)))
--- test/GraphicsEditor/test_draw.clj
+++ test/GraphicsEditor/test_draw.clj
@@ -5,9 +5,9 @@
 
 (facts "about neighbours"
        (let [image [[:O :O :O] [:O :O :O] [:O :O :O]]]
-         (neighbours [1 1]) => (just [0 1] [2 1] [1 0] [1 2])
-         (valid? [0 0]) => true
-         (valid? [-1 0]) => false
-         (valid? [0 -1]) => false
-         (neighbours [0 0]) => (just [1 0] [0 1])
+         (neighbours [1 1] [3 3]) => (just [0 1] [2 1] [1 0] [1 2])
+         (valid? [0 0] [2 3]) => true
+         (valid? [-1 0] [2 3]) => false
+         (valid? [0 -1] [2 3]) => false
+         (neighbours [0 0] [3 3]) => (just [1 0] [0 1])
          ))
