a87acd76e6083537fdec3177387b66e4ba364444
Christian Blunden <christian.blunden@gmail.com>
2011-05-15 18:08:07 +0100

    added more boundary checks to valid?

--- src/GraphicsEditor/draw.clj
+++ src/GraphicsEditor/draw.clj
@@ -3,8 +3,8 @@
 (def deltas [[-1 0] [1 0] [0 -1] [0 1]])
 
 (defn valid? [point size]
-  (and (<= 0 (first point) (first size))
-       (<= 0 (last point) (last size))))
+  (and (< -1 (first point) (first size))
+       (< -1 (last point) (last size))))
 
 (defn neighbours [point size]
   (filter (fn [new-point] (valid? new-point size))
--- test/GraphicsEditor/test_draw.clj
+++ test/GraphicsEditor/test_draw.clj
@@ -11,6 +11,11 @@
          (valid? [0 -1] [2 3]) => false
          (valid? [3 3] [2 3]) => false
          (valid? [2 4] [2 3]) => false
-         (valid? [2 3] [2 3]) => true
+         (valid? [1 2] [2 3]) => true
+         (valid? [0 0] [1 1]) => true
          (neighbours [0 0] [3 3]) => (just [1 0] [0 1])
+         (neighbours [3 2] [4 3]) => (just [2 2] [3 1])
+         (neighbours [0 0] [1 1]) => empty?
          ))
+
+(facts "about filler")
