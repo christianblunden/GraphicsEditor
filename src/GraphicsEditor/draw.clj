077f2b3acaee74ab9802e8c9788e6e334c7db97d
Christian Blunden <christian.blunden@gmail.com>
2011-05-15 16:52:29 +0100

    filter negative neighbour points

--- src/GraphicsEditor/draw.clj
+++ src/GraphicsEditor/draw.clj
@@ -2,6 +2,9 @@
 
 (def deltas [[-1 0] [1 0] [0 -1] [0 1]])
 
+(defn valid? [point]
+  (every? #(<= 0 %) point))
 
 (defn neighbours [point]
-  (map #(map + point %) deltas))
+  (filter valid?
+          (map #(map + point %) deltas)))
--- test/GraphicsEditor/test_draw.clj
+++ test/GraphicsEditor/test_draw.clj
@@ -6,5 +6,8 @@
 (facts "about neighbours"
        (let [image [[:O :O :O] [:O :O :O] [:O :O :O]]]
          (neighbours [1 1]) => (just [0 1] [2 1] [1 0] [1 2])
-
+         (valid? [0 0]) => true
+         (valid? [-1 0]) => false
+         (valid? [0 -1]) => false
+         (neighbours [0 0]) => (just [1 0] [0 1])
          ))
